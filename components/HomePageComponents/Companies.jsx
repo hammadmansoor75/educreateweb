e64061bd495d9ebe86c07cbf72d07901fed8c340
Hammad Mansoor <hammadmansoor75@gmail.com>
2024-08-17 17:17:54 +0500

    testmimonial mobile changes

--- components/HomePageComponents/Companies.jsx
+++ components/HomePageComponents/Companies.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Companies = () => {
   return (
     <section className="py-10 bg-white dark:bg-black">
-        <div className="container mx-auto bg-black w-3/4  rounded-sm">
+        <div className="hidden md:flex container mx-auto bg-black w-3/4  rounded-sm">
             <div className="flex flex-wrap justify-center animate-pulse items-center gap-5">
               <Image src='/assets/company/netflix.jpg' alt="company" height={200} width={200}/>
               <Image src='/assets/company/youtube.jpg' alt="company" height={200} width={200}/>
@@ -15,6 +15,19 @@ const Companies = () => {
               <Image src='/assets/company/lexmark.jpg' alt="company" height={200} width={200}/>
               <Image src='/assets/company/microsoft.png' alt="company" height={200} width={200}/>
             </div>  
+        </div>
+
+        <div className='flex container mx-auto bg-black rounded-sm' >
+          <div className='flex flex-wrap justify-center animate-pulse items-center gap-5'>
+              <Image src='/assets/company/netflix.jpg' alt="company" height={70} width={70}/>
+              <Image src='/assets/company/youtube.jpg' alt="company" height={70} width={70}/>
+              <Image src='/assets/company/google.png' alt="company" height={70} width={70}/>
+              <Image src='/assets/company/lenovo.png' alt="company" height={70} width={70}/>
+              <Image src='/assets/company/slack.pmg.png' alt="company" height={70} width={70}/>
+              <Image src='/assets/company/verizon.jpg' alt="company" height={70} width={70}/>
+              <Image src='/assets/company/lexmark.jpg' alt="company" height={70} width={70}/>
+              <Image src='/assets/company/microsoft.png' alt="company" height={70} width={70}/>
+          </div>
         </div>
       </section>
   )
